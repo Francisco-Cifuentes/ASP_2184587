45e373f44d0bb9930e38099d433487f3af3bbce0
Francisco-Cifuentes <facifuentes04@misena.edu.co>
2021-05-12 13:55:34 -0500

    Agregar archivos de proyecto.

---
+++ ASP_2184587/Controllers/UsuarioController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ASP_2184587.Models;
+
+namespace ASP_2184587.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        // GET: Usuario
+        public ActionResult Index()
+        {
+            using (var db = new inventarioEntities())
+            {
+                return View(db.usuario.ToList());
+            }            
+        }
+    }
+}
