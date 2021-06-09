83f71b0620d8cbd963a85a0931c0e04f058c5063
Francisco-Cifuentes <facifuentes04@misena.edu.co>
2021-06-09 11:26:35 -0500

    CRUD y Visual Tabla Cliente

---
+++ ASP_2184587/Controllers/ClienteController.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ASP_2184587.Models;
+
+namespace ASP_2184587.Controllers
+{
+    public class ClienteController : Controller
+    {
+        // GET: Cliente
+        public ActionResult Index()
+        {
+            using (var db = new inventarioEntities())
+            {
+                return View(db.cliente.ToList());
+            }
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(cliente cliente)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+
+            try
+            {
+                using (var db = new inventarioEntities())
+                {
+                    db.cliente.Add(cliente);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "Error - Datos ingresados de manera erronea");
+                return View();
+            }
+        }
+        public ActionResult Details(int id)
+        {
+            using (var db = new inventarioEntities())
+            {
+                var findUser = db.cliente.Find(id);
+                return View(findUser);
+            }
+        }
+        public ActionResult Edit(int id)
+        {
+            try
+            {
+                using (var db = new inventarioEntities())
+                {
+                    cliente findUser = db.cliente.Where(a => a.id == id).FirstOrDefault();
+                    return View(findUser);
+                }
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "Error" + ex);
+                return View();
+            }
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(cliente editUser)
+        {
+            try
+            {
+                using (var db = new inventarioEntities())
+                {
+                    cliente user = db.cliente.Find(editUser.id);
+
+                    user.nombre = editUser.nombre;
+                    user.documento = editUser.documento;
+                    user.email = editUser.email;
+
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (Exception)
+            {
+                ModelState.AddModelError("", "Error - Datos ingresados de manera erronea");
+                return View();
+            }
+        }
+        public ActionResult Delete(int id)
+        {
+            try
+            {
+                using (var db = new inventarioEntities())
+                {
+                    var findUser = db.cliente.Find(id);
+                    db.cliente.Remove(findUser);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "Error" + ex);
+                return View();
+            }
+        }
+    }
+}
