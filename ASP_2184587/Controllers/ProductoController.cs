75c6ab62b4cb2a5f27de02656678f6589675acf2
Francisco-Cifuentes <facifuentes04@misena.edu.co>
2021-06-09 13:21:08 -0500

    CRUD y Visual Tabla Producto FK

---
+++ ASP_2184587/Controllers/ProductoController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ASP_2184587.Models;
+
+namespace ASP_2184587.Controllers
+{
+    public class ProductoController : Controller
+    {
+        // GET: Producto
+        public ActionResult Index()
+        {
+            using (var db = new inventarioEntities())
+            {
+                return View(db.producto.ToList());
+            }
+        }
+
+        public static string NombreProveedor(int ? idProveedor)
+        {
+            using (var db = new inventarioEntities())
+            {
+                return db.proveedor.Find(idProveedor).nombre;
+            }
+        }
+
+        public ActionResult ListarProveedores()
+        {
+            using (var db = new inventarioEntities())
+            {
+                return PartialView(db.proveedor.ToList());
+            }
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(producto producto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+
+            try
+            {
+                using (var db = new inventarioEntities())
+                {                    
+                    db.producto.Add(producto);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "Error - Datos ingresados de manera erronea");
+                return View();
+            }
+        }
+
+        public ActionResult Details(int id)
+        {
+            using (var db = new inventarioEntities())
+            {
+                var findUser = db.producto.Find(id);
+                return View(findUser);
+            }
+        }
+
+        public ActionResult Edit(int id)
+        {
+            try
+            {
+                using (var db = new inventarioEntities())
+                {
+                    producto findUser = db.producto.Where(a => a.id == id).FirstOrDefault();
+                    return View(findUser);
+                }
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "Error" + ex);
+                return View();
+            }
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(producto editUser)
+        {
+            try
+            {
+                using (var db = new inventarioEntities())
+                {
+                    producto user = db.producto.Find(editUser.id);
+
+                    user.nombre = editUser.nombre;
+                    user.percio_unitario = editUser.percio_unitario;
+                    user.descripcion = editUser.descripcion;
+                    user.cantidad = editUser.cantidad;
+                    user.id_proveedor = editUser.id_proveedor;
+
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (Exception)
+            {
+                ModelState.AddModelError("", "Error - Datos ingresados de manera erronea");
+                return View();
+            }
+        }
+        public ActionResult Delete(int id)
+        {
+            try
+            {
+                using (var db = new inventarioEntities())
+                {
+                    var findUser = db.producto.Find(id);
+                    db.producto.Remove(findUser);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "Error" + ex);
+                return View();
+            }
+        }
+    }
+}
