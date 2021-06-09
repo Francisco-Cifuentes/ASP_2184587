fb517ef59f7ab919a7cfa5e263b136eedf212f20
Francisco-Cifuentes <facifuentes04@misena.edu.co>
2021-06-09 02:14:02 -0500

    Funcionalidad de Crear Usuario

--- ASP_2184587/Controllers/UsuarioController.cs
+++ ASP_2184587/Controllers/UsuarioController.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
+//importando los modelos de la base de datos
 using ASP_2184587.Models;
 
 namespace ASP_2184587.Controllers
@@ -15,7 +17,51 @@ namespace ASP_2184587.Controllers
             using (var db = new inventarioEntities())
             {
                 return View(db.usuario.ToList());
-            }            
+            }
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(usuario usuario)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+
+            try
+            {
+                using (var db = new inventarioEntities())
+                {
+                    usuario.password = UsuarioController.HashSHA1(usuario.password);
+                    db.usuario.Add(usuario);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "error " + ex);
+                return View();
+            }
+        }
+        public static string HashSHA1(string value)
+        {
+            var sha1 = System.Security.Cryptography.SHA1.Create();
+            var inputBytes = Encoding.ASCII.GetBytes(value);
+            var hash = sha1.ComputeHash(inputBytes);
+
+            var sb = new StringBuilder();
+            for (var i = 0; i < hash.Length; i++)
+            {
+                sb.Append(hash[i].ToString("X2"));
+            }
+            return sb.ToString();
         }
     }
 }
